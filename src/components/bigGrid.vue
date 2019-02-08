7c0c6cbb2254284db158b4d8a03a883a9b20e204
lambia <luca@lambia.it>
2019-02-08 11:41:11 +0100

    Starting

---
+++ src/components/bigGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>{{n}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bigGrid',
+  props: {
+    n: Number
+  },
+  data: function() {
+    return {
+      arrayTest: []
+    } 
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <bigGrid :n="23" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import bigGrid from '@/components/bigGrid.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    bigGrid
   }
 }
 </script>
